8b474d84447410b979c3876a845ec2060c62d226
あまみや ゆうこ <1@4321.io>
2015-06-09 19:25:12 +0800

    1

---
+++ LongjiangAgricultureCloud/Models/LongjiangAgricultureCloudContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace LongjiangAgricultureCloud.Models
+{
+    public class LongjiangAgricultureCloudContext : DbContext
+    {
+        public DbSet<Area> Areas { get; set; }
+        public DbSet<Catalog> Catalogs { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Information> Informations { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public LongjiangAgricultureCloudContext() : base("mssqldb") { }
+    }
+}
