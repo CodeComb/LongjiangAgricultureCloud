8f5c4e452fb490cd777ef14d34978067c52390cc
あまみや ゆうこ <1@4321.io>
2015-06-18 21:39:05 +0800

    1

--- LongjiangAgricultureCloud/Areas/Mobile/Controllers/MemberController.cs
+++ LongjiangAgricultureCloud/Areas/Mobile/Controllers/MemberController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using LongjiangAgricultureCloud.Schema;
 using LongjiangAgricultureCloud.Models;
+using LongjiangAgricultureCloud.Helpers;
 
 namespace LongjiangAgricultureCloud.Areas.Mobile.Controllers
 {
@@ -24,11 +25,19 @@ namespace LongjiangAgricultureCloud.Areas.Mobile.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Config(User User)
+        public ActionResult Config(User User, string Confirm)
         {
             CurrentUser.AreaID = User.AreaID;
             CurrentUser.Address = User.Address;
             CurrentUser.Name = User.Name;
+            if (!string.IsNullOrEmpty(User.Password))
+            {
+                if (Confirm != User.Password)
+                    return Msg("两次密码输入不一致，请返回重试！");
+                CurrentUser.Password = Security.SHA1(Confirm);
+            }
+            DB.SaveChanges();
+            return Msg("个人资料保存成功！");
         }
     }
 }
